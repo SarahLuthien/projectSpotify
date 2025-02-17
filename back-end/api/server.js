9f4f8ba410db2895a9ae59a4d0d71a40fef7c34f
Sarah Luthien <melian.maia.m@gmail.com>
2025-02-17 19:05:35 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,22 +1,31 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./conect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Olá mundo! Estou escrevendo");
 });
 
-app.get("/artist", async (request, response) => {
+app.get("/api/artist", async (request, response) => {
   response.send(await db.collection("artist").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../../front-end/dist")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,9 @@
+import "dotenv/config";
 import axios from "axios";
 
-const URL = "http://localhost:3000";
+const NODE_ENV = process.env.NODE_ENV;
+
+const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
 
 const responseArtist = await axios.get(`${URL}/artist`);
 const responseSongs = await axios.get(`${URL}/songs`);
