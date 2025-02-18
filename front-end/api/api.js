50f4b2c987ee3e349e510dcec349a31b8303bb43
Sarah Luthien <melian.maia.m@gmail.com>
2025-02-18 15:34:37 -0300

    ajustes no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,7 @@
 import "dotenv/config";
 import axios from "axios";
 
-const NODE_ENV = process.env.NODE_ENV;
-
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const URL = "https://projectspotify.onrender.com/api";
 
 const responseArtist = await axios.get(`${URL}/artist`);
 const responseSongs = await axios.get(`${URL}/songs`);
